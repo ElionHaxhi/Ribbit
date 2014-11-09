1ffabab41ed04027c337d160a03a4473f649e5de
elionhaxhi <elion261084@hotmail.com>
2014-11-09 20:47:41 +0100

    udp src

--- src/com/elionhaxhi/ribbit/MainActivity.java
+++ src/com/elionhaxhi/ribbit/MainActivity.java
@@ -206,7 +206,7 @@ public class MainActivity extends FragmentActivity
             // this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setIcon(mSectionsPagerAdapter.getIcon(i))
                             .setTabListener(this));
         }
     }
